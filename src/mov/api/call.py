6b68267a02eecebacbb96dcb1c1808b2f52e5525
Nicou11 <lyoungin1204@gmail.com>
2024-07-31 10:36:25 +0900

    push

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -51,10 +51,10 @@ def apply_type2df(load_dt="20120101", path="~/tmp/test_parquet"):
     df = pd.read_parquet(f'{path}/load_dt={load_dt}')
     num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt', 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 'salesChange', 'audiInten', 'audiChange']
 
-    for c in num_cols:
-        df[c] = pd.to_numeric(df[c]) 
+    #for c in num_cols:
+        #df[c] = pd.to_numeric(df[c]) 
     
-    #df[num_cols] = df[num_cols].apply(pd.to_numeric)
+    df[num_cols] = df[num_cols].apply(pd.to_numeric)
 
-    return df, num_cols
+    return df
 
